cfb0aec1211ae3de08d33a13fac046bb18396913
nasir-buddy <syedali172000@gmail.com>
2024-12-30 15:11:52 +0530

    fix: handle optional chaining for authorization header in auth middleware

--- Backend/middleware/auth.middleware.js
+++ Backend/middleware/auth.middleware.js
@@ -4,7 +4,7 @@ const jwt = require('jsonwebtoken');
 
 module.exports.authUser = async (req, res, next) => {
     // getting the token from cookies if not then from header 
-    const token = req.cookies.token || req.headers.authorization.split(' ')[1];
+    const token = req.cookies.token || req.headers.authorization?.split(' ')[1];
     // checking the token if not available then return with 401 status
     if (!token) {
         return res.status(401).json({ message: 'Unauthorized' });
