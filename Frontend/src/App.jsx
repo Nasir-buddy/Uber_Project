57f4715d490d4afc7c400564eeb9e6be5a9a29f1
nasir-buddy <syedali172000@gmail.com>
2025-01-02 23:18:57 +0530

    feat: add routes for UserSignup, CaptainLogin, and CaptainSignup pages

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import Home from './pages/Home'
 import { Route, Routes } from 'react-router-dom'
+import UserSignup from './pages/UserSignup'
+import CaptainLogin from './pages/CaptainLogin'
+import CaptainSignup from './pages/CaptainSignup'
 
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path='/login' element={<UserSignup />} />
+      <Route path='/signup' element={<UserSignup />} />
+      <Route path='/captain-login' element={< CaptainLogin />} />
+      <Route path='/captain-signup' element={<CaptainSignup />} />
     </Routes>
   )
 }
