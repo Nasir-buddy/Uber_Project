6395026f4fe8c04d022ca501b82780782c16b586
nasir-buddy <syedali172000@gmail.com>
2025-01-03 14:43:13 +0530

    feat & fix: implement UserLogin page with form and styling fix routing in app.js

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -4,13 +4,14 @@ import { Route, Routes } from 'react-router-dom'
 import UserSignup from './pages/UserSignup'
 import CaptainLogin from './pages/CaptainLogin'
 import CaptainSignup from './pages/CaptainSignup'
+import UserLogin from './pages/UserLogin'
 
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path='/login' element={<UserSignup />} />
+      <Route path='/login' element={<UserLogin />} />
       <Route path='/signup' element={<UserSignup />} />
       <Route path='/captain-login' element={< CaptainLogin />} />
       <Route path='/captain-signup' element={<CaptainSignup />} />
--- Frontend/src/pages/UserLogin.jsx
+++ Frontend/src/pages/UserLogin.jsx
@@ -2,7 +2,20 @@ import React from 'react'
 
 const UserLogin = () => {
   return (
-    <div>UserLogin</div>
+    <div className='p-7'>
+      <img className='w-16 mb-7' src="https://logodownload.org/wp-content/uploads/2015/05/uber-logo-7.png" alt="" />
+      <form action="" >
+        <h3 className='text-xl mb-2 font-medium'>What's your email</h3>
+        <input
+          className='bg-[#eeeeee] rounded px-4 py-2 mb-7 border w-full text-lg placeholder:text-base'
+          required type="email" placeholder="email@example.com" />
+        <h3 className='text-xl mb-2 font-medium'>What's your password</h3>
+        <input
+          className='bg-[#eeeeee] rounded px-4 py-2 mb-7 border w-full text-lg placeholder:text-base'
+          required type="password" placeholder="********" />
+        <button className='w-full bg-black text-white py-5 mt-2 rounded flex items-center justify-center'>Continue</button>
+      </form>
+    </div>
   )
 }
 
