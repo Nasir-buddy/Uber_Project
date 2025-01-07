d7447ea80b299b0bf2d273b9f2f6f53d1e05e487
nasir-buddy <syedali172000@gmail.com>
2025-01-07 21:15:06 +0530

    feat: add vehicle selection section to Home component with pricing and details

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -36,14 +36,13 @@ const Home = () => {
     }
   }, [panelOpen])
   return (
-    <div className='h-screen relative'>
+    <div className='h-screen relative overflow-hidden'>
       <img className='w-16 mb-7 absolute left-5 top-5' src="https://logodownload.org/wp-content/uploads/2015/05/uber-logo-7.png" alt="" />
       <div className='h-screen w-screen'>
         {/* image for temporary uses */}
         <img className='h-full w-full object-cover'
           src="https://lh3.googleusercontent.com/proxy/GH6cV2LdByuyV50BDypx-KHuQokGS5hH7IRAGva1unyEXE3X2UXvmcSrlCkDTSRKOEoCzi3dJe3qjJV0OKBVJfaiNM7-EhddY8MIqZmQWkn--tksiio7SZ4SjooC5pAu9GMIUW0gl5yf=w1200-h630-p-k-no-nu" alt="" />
       </div>
-
       <div className='h-screen flex flex-col justify-end absolute w-full top-0  rounded-t-lg'>
         <div className=' bg-white h-[20%] p-5 relative'>
           <h5
@@ -85,8 +84,38 @@ const Home = () => {
           {<LocationSearchPanel />}
         </div>
       </div>
+      <div className='fixed x-10 bottom-0 bg-white w-full py-3 px-3'>
+        <h3 className='text-xl font-semibold'>Choose a Vehicle</h3>
+          <div className='flex items-center justify-between p-3 my-4 w-full border-2 active:border-black transition-all duration-75 rounded-xl'>
+              <img className='h-16 bg-transparent ' src="https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,h_368,w_552/v1631132572/assets/ff/62c74e-2819-46c9-83b6-0d9a8e4fe238/original/uber_green.png" alt="" />
+              <div className='w-1/2'>
+                <h4 className='font-medium'>Uber GO <i className="ri-user-2-fill text-sm">4</i></h4>
+                <h5 className='font-medium text-sm'>2 mins away</h5>
+                <p className='font-medium text-xs text-gray-400'>Affordable, compact rides</p>
+              </div>
+              <h2 className='text-2xl font-semibold'>Rs.195</h2>
+          </div>
 
+          <div className='flex items-center justify-between p-3 my-4 w-full border-2 active:border-black transition-all duration-75 rounded-xl'>
+              <img className='h-16 bg-transparent ' src="https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,h_368,w_552/v1649231091/assets/2c/7fa194-c954-49b2-9c6d-a3b8601370f5/original/Uber_Moto_Orange_312x208_pixels_Mobile.png" alt="" />
+              <div className='w-1/2'>
+                <h4 className='font-medium'>Uber Bike <i className="ri-user-2-fill text-sm">4</i></h4>
+                <h5 className='font-medium text-sm'>1 mins away</h5>
+                <p className='font-medium text-xs text-gray-400'>Affordable, compact rides</p>
+              </div>
+              <h2 className='text-2xl font-semibold'>Rs.23</h2>
+          </div>  
 
+          <div className='flex items-center justify-between p-3 my-4 w-full border-2 active:border-black transition-all duration-75 rounded-xl'>
+              <img className='h-16 bg-transparent ' src="https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,h_368,w_552/v1648431773/assets/1d/db8c56-0204-4ce4-81ce-56a11a07fe98/original/Uber_Auto_558x372_pixels_Desktop.png" alt="" />
+              <div className='w-1/2'>
+                <h4 className='font-medium'>Uber Bike <i className="ri-user-2-fill text-sm">4</i></h4>
+                <h5 className='font-medium text-sm'>1 mins away</h5>
+                <p className='font-medium text-xs text-gray-400'>Affordable, compact rides</p>
+              </div>
+              <h2 className='text-2xl font-semibold'>Rs.23</h2>
+          </div>         
+      </div>
     </div>
   )
 }
