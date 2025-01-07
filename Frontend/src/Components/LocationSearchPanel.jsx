fbf1bba9c731153ae948d010403d4751c2454ba8
nasir-buddy <syedali172000@gmail.com>
2025-01-07 21:59:59 +0530

    feat: integrate vehicle selection functionality into LocationSearchPanel and Home component

--- Frontend/src/Components/LocationSearchPanel.jsx
+++ Frontend/src/Components/LocationSearchPanel.jsx
@@ -2,7 +2,8 @@ import React, { useEffect } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 
-const LocationSearchPanel = () => {
+const LocationSearchPanel = (props) => {
+    
     // sample array of locations 
     const location = ["Accurate institute of management and technology. Greater Noida, Uttar Pradesh.",
         "GNIOT. Greater Noida, Uttar Pradesh.",
@@ -16,15 +17,18 @@ const LocationSearchPanel = () => {
             {
                 location.map((item, index) => {
                     return (
-                        <div className='flex items-center  border-2 border-white active:border-black transition-all duration-100 p-3 rounded-lg justify-start gap-4 my-4'>
-                            <div className='w-12'> 
-                            <h2 className='bg-gray-300 flex items-center justify-center h-10 rounded-full '>
-                                <i className="ri-map-pin-fill"></i>
-                            </h2>
+                        <div key={index} onClick={()=>{
+                            props.setvehiclePanel(true);
+                            props.setpanelOpen(false);
+                        }} className='flex items-center  border-2 border-white active:border-black transition-all duration-100 p-3 rounded-lg justify-start gap-4 my-4'>
+                            <div className='w-12'>
+                                <h2 className='bg-gray-300 flex items-center justify-center h-10 rounded-full '>
+                                    <i className="ri-map-pin-fill"></i>
+                                </h2>
                             </div>
-                           <div className='w-full'>
-                           <h4 className='text-md'>{item}</h4>
-                           </div>
+                            <div className='w-full'>
+                                <h4 className='text-md'>{item}</h4>
+                            </div>
                         </div>
                     )
                 })
