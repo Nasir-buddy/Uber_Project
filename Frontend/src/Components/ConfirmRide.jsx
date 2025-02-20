5baf6d5fc69b14883f079da668dbab045e957e41
Nasir-Buddy <syedali172000@gmail.com>
2025-02-21 01:56:48 +0530

    feat: add onClick handler to close confirm ride panel in ConfirmRide component

--- Frontend/src/Components/ConfirmRide.jsx
+++ Frontend/src/Components/ConfirmRide.jsx
@@ -3,7 +3,11 @@ import React from 'react'
 const ConfirmRide = (props) => {
     return (
         <div>
-            <div className='py-1 w-full flex justify-center'>
+            <div className='py-1 w-full flex justify-center'
+            onClick={() => {
+                props.setconfirmRidePanel(false);
+            }}
+            >
                 <i className="ri-arrow-down-wide-fill"></i>
             </div>
             <h3 className='text-xl font-semibold'>Confirm Vehicle</h3>
