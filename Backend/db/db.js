4b986e84935a22a0373d19a4efa8fdbc4638514c
Nasir-Buddy <syedali172000@gmail.com>
2025-02-22 17:10:12 +0530

    feat: update database connection log message for clarity

--- Backend/db/db.js
+++ Backend/db/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 function connectToDb() {
     // connecting to the database.
     mongoose.connect(process.env.DB_CONNECTION)
-    .then(()=>{console.log("connected to db");
+    .then(()=>{console.log("connected to MongoDB Atlas server...");
     })
     .catch((err)=>{console.log("error in connecting to db", err);
     })
